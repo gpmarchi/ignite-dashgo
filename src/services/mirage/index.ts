2ffc594f6577b8b00108c911da6472ad0d0776e8
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2022-01-13 14:38:07 -0300

    feat: Use factory/seed to generate fake user data

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,6 @@
+import faker from 'faker';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { createServer, Model } from 'miragejs';
+import { createServer, Factory, Model } from 'miragejs';
 
 type User = {
   name: string;
@@ -11,6 +12,24 @@ export function makeServer() {
   const server = createServer({
     models: {
       user: Model.extend<Partial<User>>({}),
+    },
+
+    factories: {
+      user: Factory.extend({
+        name(index: number) {
+          return `User ${index + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+
+    seeds(mirageServer) {
+      mirageServer.createList('user', 200);
     },
 
     routes() {
