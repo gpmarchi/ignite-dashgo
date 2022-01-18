ef1de85206cf85bd1c03914163f1fbf57bb479f9
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2022-01-18 15:19:34 -0300

    feat: Enable page changing

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import {
@@ -25,7 +26,8 @@ import { Sidebar } from '../../components/Sidebar';
 import { useUsers } from '../../services/hook/useUsers';
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -83,7 +85,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(user => (
+                  {data.users.map(user => (
                     <Tr key={user.id}>
                       <Td px={['4', '4', '6']}>
                         <Checkbox colorScheme="pink" />
@@ -116,9 +118,9 @@ export default function UserList() {
               </Table>
 
               <Pagination
-                totalCountOfRecords={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRecords={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
