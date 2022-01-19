6287bb94ae30e9e3922d760ec9fef6a61259dec0
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2022-01-19 11:48:18 -0300

    feat: Prefetch individual user data on link hover

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { useState } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
@@ -18,12 +18,15 @@ import {
   Text,
   useBreakpointValue,
   Spinner,
+  Link,
 } from '@chakra-ui/react';
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
 import { useUsers } from '../../services/hook/useUsers';
+import { queryClient } from '../../services/queryClient';
 
 export default function UserList() {
   const [page, setPage] = useState(1);
@@ -33,6 +36,20 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  async function handleUserPrefetch(userId: number) {
+    await queryClient.prefetchQuery(
+      ['user', userId],
+      async () => {
+        const response = await api.get(`/users/${userId}`);
+
+        return response.data;
+      },
+      {
+        staleTime: 10 * 60 * 1000, // 10 minutes
+      }
+    );
+  }
 
   return (
     <Box>
@@ -50,7 +67,7 @@ export default function UserList() {
               )}
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -60,7 +77,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           {isLoading ? (
@@ -92,7 +109,12 @@ export default function UserList() {
                       </Td>
                       <Td>
                         <Box>
-                          <Text fontWeight="bold">{user.name}</Text>
+                          <Link
+                            color="purple.400"
+                            onMouseEnter={() => handleUserPrefetch(user.id)}
+                          >
+                            <Text fontWeight="bold">{user.name}</Text>
+                          </Link>
                           <Text fontSize="sm" color="gray.300">
                             {user.email}
                           </Text>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -54,6 +54,8 @@ export function makeServer() {
 
       this.post('/users');
 
+      this.get('/users/:id');
+
       this.namespace = '';
       this.passthrough();
     },
